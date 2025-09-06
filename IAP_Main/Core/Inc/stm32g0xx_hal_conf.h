0c69dd5def0268ed78d008f7b82d61124735e48e
saminsarabadani <125192363+saminsarabadani@users.noreply.github.com>
2025-09-06 20:49:45 +0330

    Update stm32g0xx_hal_conf.h

--- IAP_Main/Core/Inc/stm32g0xx_hal_conf.h
+++ IAP_Main/Core/Inc/stm32g0xx_hal_conf.h
@@ -1,22 +1,3 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file    stm32g0xx_hal_conf.h
-  * @author  MCD Application Team
-  * @brief   HAL configuration file.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2018 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef STM32G0xx_HAL_CONF_H
